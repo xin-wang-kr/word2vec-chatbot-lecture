e9023b3d992e8c2cb41d6d9e3efdba908aef8487
Vision(Xin) Wang <xinwang35314@gmail.com>
2024-02-22 16:35:58 -0500

    Update assignment_1.py

--- assignment_1.py
+++ assignment_1.py
@@ -1,3 +1,14 @@
+### Assignment instruction
+# The goal of this assignment is to create a word2vec-based question-answer chatbot application that should be able
+# to give the best answer based on vector search toward both question set and answer set. 
+# Our exercise only showed how to apply question set for vector search. You can follow the hints to generate the chatbot. 
+# What you need to submit for this assignment: an app url (You should publish your chatbot application on Streamlit Cloud. 
+# Your chatbot assignment will be evaluated based on three query questions:
+# (1) A year before improving and popularizing the electrophorus, what did Volta become?
+# (2) Does the Hymenoptera order include ants?
+# (3) Who invented the voltaic pile?
+###
+
 import streamlit as st
 import pandas as pd
 import faiss
@@ -7,13 +18,11 @@ import numpy as np
 # load question-answer dataset 
 df = pd.read_csv("data/Question_Answer_Dataset_v1.2_S10.csv")
 
-# load question and answer vectors
-vector = np.load('data/vector.npz')
-ques_vec = vector['x']
-ans_vec = vector['y']
+# load question vector
+ques_vec = np.loadtxt('data/ques_vec.csv', delimiter=',')
 
 # load th trained word2vec model 
-trained_w2v = gensim.models.Word2Vec.load("data/w2v-advanced.model")
+trained_w2v = gensim.models.Word2Vec.load("data/w2v.model")
 
 # App title
 st.set_page_config(page_title="Word2vec Question and Answer Chatbot")
@@ -24,16 +33,24 @@ st.image("data/header-chat-box.png")
 # chat title 
 st.title("Word2vec Question and Answer Chatbot")
 
-#st.session_state.messages = [] # reset session state
-
 # Store generated responses
 if "messages" not in st.session_state.keys():
     st.session_state.messages = [{"role": "assistant", "content": "How may I help you?"}]
 #else:
-  #  st.session_state.pop('messages') # reset session state
+    #st.session_state.pop('messages') # reset session state
    # st.session_state.messages = [{"role": "assistant", "content": "How may I help you?"}]
 
 # Display chat messages
+#for message in st.session_state.messages:
+    #with st.chat_message(message["role"]):
+        #st.write(message["content"])
+#    if ".png" in message["content"]:
+#        with st.chat_message(message["role"]):
+#            st.image(message["content"])
+ #   else:
+#        with st.chat_message(message["role"]):
+ #           st.write(message["content"])
+
 for message in st.session_state.messages:
 	if type(message["content"]) == str:
 		with st.chat_message(message["role"]):
@@ -48,35 +65,26 @@ def trained_sentence_vec(sent):
     # Filter out terms that are not in the vocabulary from the question sentence
     qu_voc = [tm for tm in sent if tm in trained_w2v.wv]
     # Get the embedding of the characters
-    emb = np.vstack([trained_w2v.wv[tm] for tm in qu_voc])
+    emb = np.vstack([trained_w2v.wv[tm] for tm in sent if tm in trained_w2v.wv])
     # Calculate the vectors of each included word to get the vector of the question
     ave_vec = np.mean(emb, axis=0)
     return ave_vec
 
-def find_answer(qr_sentence, ques_vec, ans_vec):
+def find_answer(qr_sentence, ques_vec):
     # use one query sentence to retrieve answer
     qr_sentence = gensim.utils.simple_preprocess(qr_sentence)
     qr_sent_vec = trained_sentence_vec(qr_sentence)
 
     # perform vector search through similarity comparison
     n_dim = ques_vec.shape[1]
-    n_q_a = ques_vec.shape[0] # number of pairs of question and answer
     x = np.vstack(ques_vec).astype(np.float32)
-    y = np.vstack(ans_vec).astype(np.float32)
     q = qr_sent_vec.reshape(1, -1)
     index = faiss.index_factory(n_dim, "Flat", faiss.METRIC_INNER_PRODUCT)
-    # add all questions
     faiss.normalize_L2(x)
     index.add(x)
-    # add all answers
-    faiss.normalize_L2(y)
-    index.add(y)
-    # do vector search for the query sentence
     faiss.normalize_L2(q)
     similarity, idx = index.search(q, k=index.ntotal)
     ans_idx = idx[0][0]
-    if ans_idx >= n_q_a:
-      ans_idx = ans_idx - n_q_a
     return ans_idx
 
 
@@ -90,8 +98,9 @@ if prompt := st.chat_input("What's your question?"):
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-            ans_idx = find_answer(prompt, ques_vec, ans_vec)
+            ans_idx = find_answer(prompt, ques_vec)
             response = df["Answer"][ans_idx]
             st.write(response)
+            #st.image("data/header-chat-box.png")
     message = {"role": "assistant", "content": response}
     st.session_state.messages.append(message)
