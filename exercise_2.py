d3a08e10aeec8babd9c7afce4cdab6c35f7f002a
Vision(Xin) Wang <xinwang35314@gmail.com>
2024-02-26 14:16:08 -0500

    Update exercise_2.py

--- exercise_2.py
+++ exercise_2.py
@@ -1,14 +1,3 @@
-### Assignment instruction
-# The goal of this assignment is to create a word2vec-based question-answer chatbot application that should be able
-# to give the best answer based on vector search toward both question set and answer set. 
-# Our exercise only showed how to apply question set for vector search. You can follow the hints to generate the chatbot. 
-# What you need to submit for this assignment: an app url (You should publish your chatbot application on Streamlit Cloud. 
-# Your chatbot assignment will be evaluated based on three query questions:
-# (1) A year before improving and popularizing the electrophorus, what did Volta become?
-# (2) Does the Hymenoptera order include ants?
-# (3) Who invented the voltaic pile?
-###
-
 import streamlit as st
 import pandas as pd
 import faiss
@@ -18,11 +7,9 @@ import numpy as np
 # load question-answer dataset 
 df = pd.read_csv("data/Question_Answer_Dataset_v1.2_S10.csv")
 
-# load question and answer vectors
-
+# load question vector
 vector = np.load('data/vector.npz')
 ques_vec = vector['x']
-ans_vec = vector['y']
 
 # load th trained word2vec model 
 trained_w2v = gensim.models.Word2Vec.load("data/w2v.model")
@@ -39,31 +26,25 @@ st.title("Word2vec Question and Answer Chatbot")
 # Store generated responses
 if "messages" not in st.session_state.keys():
     st.session_state.messages = [{"role": "assistant", "content": "How may I help you?"}]
+#else:
+    #st.session_state.pop('messages') # reset session state
+   # st.session_state.messages = [{"role": "assistant", "content": "How may I help you?"}]
 
-# Display chat messagess
+# Display chat messages
 for message in st.session_state.messages:
-	if type(message["content"]) == str:
-		with st.chat_message(message["role"]):
-			st.write(message["content"])
-	else:
-		with st.chat_message(message["role"]):
-			st.image(message["content"])
+    with st.chat_message(message["role"]):
+	st.write(message["content"])
 
-
-# Function to generate the embedding for query question
+# Function for generating response for query question
 def trained_sentence_vec(sent):
     # Filter out terms that are not in the vocabulary from the question sentence
-    # Hint: Use model.wv to get the whole vocabulary
-    qu_voc = ...
+    qu_voc = [tm for tm in sent if tm in trained_w2v.wv]
     # Get the embedding of the characters
-    # Hint: Stack arrays in sequence vertically using np.vstack
-    emb = ...
-    # Calculate the arithmetic mean for the vectors of each included word along the column 
-    # to get the vector of the question
-    ave_vec = ...
+    emb = np.vstack([trained_w2v.wv[tm] for tm in sent if tm in trained_w2v.wv])
+    # Calculate the vectors of each included word to get the vector of the question
+    ave_vec = np.mean(emb, axis=0)
     return ave_vec
 
-# Function to find the answer through vector search
 def find_answer(qr_sentence, ques_vec):
     # use one query sentence to retrieve answer
     qr_sentence = gensim.utils.simple_preprocess(qr_sentence)
@@ -81,7 +62,6 @@ def find_answer(qr_sentence, ques_vec):
     ans_idx = idx[0][0]
     return ans_idx
 
-
 # User-provided prompt
 if prompt := st.chat_input("What's your question?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
@@ -95,6 +75,6 @@ if st.session_state.messages[-1]["role"] != "assistant":
             ans_idx = find_answer(prompt, ques_vec)
             response = df["Answer"][ans_idx]
             st.write(response)
-            
     message = {"role": "assistant", "content": response}
     st.session_state.messages.append(message)
+
